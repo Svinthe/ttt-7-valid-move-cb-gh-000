6199eabc75cf0b3dac4ff38c6746ebec3d76d23d
Umair Ahmad <ahmadumair17@gmail.com>
2017-12-02 15:47:34 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,11 @@
 
 def valid_move?(board, index)
   if index.between?(0,8)
-    position_taken?(board[index])
+    if !position_taken?(board[index])
+      true
+    else
+      false
+    end
   else
     false
   end
